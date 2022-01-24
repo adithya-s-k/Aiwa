45ec0fe1626c6c332990e4a888c8561f33b5de2c
Adithya S K <adithyaskolavi@gmail.com>
2022-01-24 22:56:11 +0530

    Blink detector and posture detector combined - Adithya S K

--- CounterModule.py
+++ CounterModule.py
@@ -1011,6 +1011,14 @@ def jump_counter(jump_goal):
     cv2.destroyAllWindows()
 
 def posture_detector_advanced():
+    detector = FaceMeshDetector(maxFaces=1)
+
+    idList = [22, 23, 24, 26, 110, 157, 158, 159, 160, 161, 130, 243]
+    ratioList = []
+    blinkCounter = 0
+    counter = 0
+    color = (255, 0, 255)
+
     timer = 0
     distance_cal = 0
     distancePoints = []
@@ -1084,7 +1092,7 @@ def posture_detector_advanced():
     with mp_pose.Pose(min_detection_confidence=0.5, min_tracking_confidence=0.5) as pose:
         while cap.isOpened():
             ret, frame = cap.read()
-            
+
             image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
             image.flags.writeable = False
 
@@ -1105,6 +1113,47 @@ def posture_detector_advanced():
                 print(distance_cal)
             except:
                 pass
+
+            if cap.get(cv2.CAP_PROP_POS_FRAMES) == cap.get(cv2.CAP_PROP_FRAME_COUNT):
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)
+
+            success, img = cap.read()
+            img, faces = detector.findFaceMesh(img, draw=False)
+
+            if faces:
+                face = faces[0]
+                for id in idList:
+                    cv2.circle(img, face[id], 3,color, cv2.FILLED)
+
+                leftUp = face[159]
+                leftDown = face[23]
+                leftLeft = face[130]
+                leftRight = face[243]
+                lenghtVer, _ = detector.findDistance(leftUp, leftDown)
+                lenghtHor, _ = detector.findDistance(leftLeft, leftRight)
+
+                # cv2.line(img, leftUp, leftDown, (0, 200, 0), 3)
+                # cv2.line(img, leftLeft, leftRight, (0, 200, 0), 3)
+
+                ratio = int((lenghtVer / lenghtHor) * 100)
+                ratioList.append(ratio)
+                if len(ratioList) > 3:
+                    ratioList.pop(0)
+                ratioAvg = sum(ratioList) / len(ratioList)
+
+                if ratioAvg < 40 and counter == 0:
+                    blinkCounter += 1
+                    color = (0,200,0)
+                    counter = 1
+                if counter != 0:
+                    counter += 1
+                    if counter > 10:
+                        counter = 0
+                        color = (255,0, 255)
+
+            cvzone.putTextRect(image, f'Blink Count: {blinkCounter}', (50, 100),
+                                    colorR=color)
+
             cv2.rectangle(image, (380,0), (840,60), (0,0,0), -1)
             cv2.putText(image, 'POSTURE DETECTION', (400,40), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2, (255,255,255), 1, cv2.LINE_AA)
 
